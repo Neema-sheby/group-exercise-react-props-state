5f1f741849ea763f0b60298cd0ae379827486e08
neema <neemadas@gmail.com>
2023-01-26 14:00:59 +0000

    Initial commit: Initial commit of the project
    Requirement 1:Displayed Chuck greetings in chuck_card.ts and also a picture of Chuck.
    
    Requirement 2: Displayed chuck informatioion. In `chuck_info.tsx, used props to display how many whales Chuck has saved and how many round house kicks he has done.
    
    Requirement 3: Displayed some jokes about Chuck.
    
    Extension: Created a variable called `const filteredJokes`, assigned it with the filtered joke of id 3 and deisplayed it in browser.
    
    Added-by: Neema Sheby
    Added-on: 26 Jan 2023

--- src/components/chuck_card.tsx
+++ src/components/chuck_card.tsx
@@ -1,14 +1,18 @@
 // 💡 You can import assets like images directly and use them in your components:
-import ChuckImage from '../assets/chuck_norris.jpg';
+import ChuckImage from "../assets/chuck_norris.jpg";
 
-const ChuckCard = () =>
-	// 💡 This weird-looking `<>` thing is a "Fragment". Each component must have exactly one child.
-	// 💡 As a result, sometimes it is useful to wrap components in Fragments like this:
-	<>
-		<h2>{ }</h2>
+interface ChuckCardProp {
+  greeting: string;
+}
 
-		<img src={'Chuck image here!'} alt='A handsome man' />
-	</>
+const ChuckCard: React.FC<ChuckCardProp> = ({ greeting }) => (
+  // 💡 This weird-looking `<>` thing is a "Fragment". Each component must have exactly one child.
+  // 💡 As a result, sometimes it is useful to wrap components in Fragments like this:
+  <>
+    <h2>{greeting}</h2>
 
+    <img src={ChuckImage} alt="A handsome man" />
+  </>
+);
 
 export default ChuckCard;
